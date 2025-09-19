b2eb28a467e74317e78fa07b1822e2696d765722
rita12356 <wadeew2016@yandex.ru>
2025-09-19 15:22:34 +0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,130 @@
+
+let input = document.querySelector(`.search-input`);
+let button = document.querySelector('.order-button');
+
+
+input.addEventListener("input", function (evt) {
+  let search = evt.target.value.toLowerCase();
+  let hasVisibleItems = false; 
+
+  for (let i = 0; i < card.length; i++) {
+    let cards = card[i];
+    let title = cards.textContent.toLowerCase(); 
+
+    if (title.includes(search)) {
+      cards.classList.remove("d-none");
+      hasVisibleItems = true; 
+    } else  {
+      cards.classList.add("d-none");
+        
+      
+    }
+  }
+
+  
+  if (!hasVisibleItems) {
+    container.innerHTML = "На странице ничего не найдено.";
+    
+  } //else if(hasVisibleItems) {
+    
+      //for(let i = 0; i < card.length; i++) {
+    
+        //  let cards = card[i];
+          //cards.classList.remove(`d-none`)
+      //}
+    
+  //}
+ 
+  
+});
+   
+
+
+let prices = [ 200, 1500, 25000, 1200, 3000, 5000];
+let available = [false, true, true, true, true, false];
+// контайнер товаров
+let container = document.querySelector(`.good-container`);
+
+let card = document.querySelectorAll(`.cards`);
+let titleArr = document.querySelectorAll(`.good-title`);
+let sidebartotal = document.querySelector(`.sidebar-total`);
+let sidebarlist = document.querySelector(`.sidebar-list`);
+
+
+let total = 0;
+let basket = [];
+
+function renderBasket(items) {
+    
+    sidebarlist.innerHTML = ``;
+    sidebartotal.innerHTML = `Итого: ${total}`;
+    for(let i = 0; i < items.length; i++) {
+        sidebarlist.innerHTML += `<li class="item">${items[i]}</li>`
+         
+    }
+}
+
+ for(let i = 0; i < card.length; i++) {
+    
+    let cards = card[i];
+    let itemBasket = titleArr[i].innerHTML;
+  
+    
+        cards.addEventListener(`click`, function() {
+            cards.classList.toggle(`card-active`);
+            if(cards.classList.contains(`card-active`)) {
+                total += prices[i]
+                basket.push(itemBasket);
+                renderBasket(basket)
+
+            } else{
+                
+                let removeIndex = basket.indexOf(itemBasket);
+                basket.splice(removeIndex, 1);
+                total -= prices[i];
+                renderBasket(basket);
+                
+                
+            }
+        
+           
+        });
+        sidebartotal.innerHTML = `Итого: ${total }`;
+    }  
+
+     button.addEventListener(`click`, function() {
+         for(let i = 0; i < card.length; i++) {
+    
+          let cards = card[i];
+    
+  
+        if(cards.classList.contains(`card-active`)) {
+        sidebarlist.innerHTML = `Заказ собран.`
+        } else{
+            sidebarlist.innerHTML = `Соберите заказ.`
+        }
+    }
+         
+        });
+    
+    
+
+    let sidebar = document.querySelector(`.sidebar`);
+let cart = document.querySelector(`.cart-button`);
+
+    cart.addEventListener(`click`, function() {
+        
+        sidebar.classList.toggle(`hidden`);
+    });
+
+    let titles = [
+        `Кактус`,
+        `Настенные часы`,
+        `Телевизор`,
+        `Блендер`,
+        `Шторы`,
+        `Комплект посуды`,
+    ];
+
+
+ 
